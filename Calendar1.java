933786f32368184b843ae56e7107e565be9d6751
Shir Amouyal <154405205+ShirAmouyal@users.noreply.github.com>
2024-01-11 14:21:52 +0200

    Update Calendar1.java

--- Calendar1.java
+++ Calendar1.java
@@ -19,12 +19,12 @@ public class Calendar1 {
 	    {
 			advance();		
         }
-		System.out.println("During the 20th century, "+countsunday+" Sundays fell on the first day of the");
+		System.out.println("During the 20th century, "+countsunday+" Sundays fell on the first day of the month");
 	 	//// Write the necessary ending code here
 	 }
 	 private static void advance() {
 		// Replace this comment with your code
-	  if(dayOfWeek==1&&month==1) countsunday++;
+	  if(dayOfWeek==1&&month==1) {countsunday++;}
 	  if(dayOfWeek==7){
 		dayOfWeek=1;
 		
